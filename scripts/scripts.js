190fc3d745e9e4d599f92e1cd080274fcb6f0ec7
StellaLB372 <249901@portlandschools.org>
2026-09-18 14:46:35 -0400

    added new stuff

---
+++ scripts/scripts.js
@@ -0,0 +1,47 @@
+//String
+console.log("10")
+console.log("Portland High School")
+
+//Numbers
+console.log(10)
+console.log (10.54)
+
+//Booleans
+console.log(true)
+console.log(false)
+
+
+
+
+
+
+//Variables are used to store values for later use
+//must choose "let" (the value may change)
+//"const" (the value will remaine the same)
+
+
+let middleSchool = 'king ms'
+
+console.log(schoolName)
+console.log(middleSchool)
+
+//schoolName = Deering High School
+//middleSchool = Lyman Moore MS
+
+
+
+
+//Interactive User Input
+
+// alert(Message).     -> creates a popup window with message in it
+//alert('Welcome to my website')
+
+// input(message).     -> create a popup with message and entry widget
+const response1 = prompt("What is your name")
+const response2 = promet("What is your favorite food?")
+
+
+
+
+
+alert("Thanks " + userName + "Enjoy ypur weekend!")
